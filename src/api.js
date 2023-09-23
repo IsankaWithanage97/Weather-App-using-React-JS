9e34ac34fb20b8e2e52e9d604079604155f84f63
Isanka Withanage <88421491+IsankaWithanage97@users.noreply.github.com>
2023-09-23 15:09:48 +0530

    uploaded only src folder

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0dc7e52320134cce5271257c568a527c";
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '02a8f93211msh221305333db3d0fp1960c9jsn8d10d10acf69',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+	}
+};
+
